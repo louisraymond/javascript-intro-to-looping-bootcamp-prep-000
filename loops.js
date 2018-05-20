66aafc816ef1d938ea715c777dba36ebc0e1eee8
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-05-20 06:29:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
-
-  // EXAMPLE
-  
-for (var i=1; i<100;i++){
-  console.log("Hello World the" +i +"time");
+function forloop(array){
+  for(var i=0; i<25;i++){
+    if i=1 {
+    array.unshift("I am 1 strange loop")
+  else 
+  array.unshift("I am ${i} strange loops")}
+  return array
 }
